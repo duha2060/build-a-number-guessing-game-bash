05619ee5d357b62ed94e1746d8e058c5c4c8739b
duha2060 <maksimov2060@gmail.com>
2023-02-08 10:33:40 +0000

    feat: add random number

--- number_guess.sh
+++ number_guess.sh
@@ -2,13 +2,17 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guess --no-align --tuples-on -c"
 
+NUMBER=$(( $RANDOM % 1000 + 1 ))
 echo "Enter your username:"
-read USER
+read USERNAME
 
-USERNAME=$($PSQL "select name from users where name='$USER'")
-
-if [[ -z $USERNAME ]]
+USER=$($PSQL "select name from users where name='$USERNAME'")
+echo $USER
+if [[ -z $USER ]]
 then
-  ADD_USERNAME=$($PSQL "INSERT INTO users(name) VALUES($USERNAME)")
+  ADD_USERNAME=$($PSQL "INSERT INTO users(name) VALUES('$USERNAME')")
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
 else
   echo "Welcome back, $USERNAME! You have played <games_played> games, and your best game took <best_game> guesses."
+fi
+
