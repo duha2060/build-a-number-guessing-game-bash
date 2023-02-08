901df9cfdaed7f0a88230dd80758e9afba67bfae
duha2060 <maksimov2060@gmail.com>
2023-02-08 10:17:41 +0000

    feat: search user

--- number_guess.sh
+++ number_guess.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+PSQL="psql --username=freecodecamp --dbname=number_guess --no-align --tuples-on -c"
+
+echo "Enter your username:"
+read USER
+
+USERNAME=$($PSQL "select name from users where name='$USER'")
+
+if [[ -z $USERNAME ]]
+then
+  ADD_USERNAME=$($PSQL "INSERT INTO users(name) VALUES($USERNAME)")
+else
+  echo "Welcome back, $USERNAME! You have played <games_played> games, and your best game took <best_game> guesses."
